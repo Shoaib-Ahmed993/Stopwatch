b693b8b7cd4fc6b5bd016e79f6f9581a1bb14cc3
Shoaib-Ahmed993 <hussainshoaib46@yahoo.com>
2020-07-08 23:54:02 +0500

    Stopwatch done

---
+++ app.js
@@ -0,0 +1,36 @@
+var min = 0;
+var sec = 0;
+var msec = 0;
+var interval;
+var minHeading = document.getElementById("min");
+var secHeading = document.getElementById("sec");
+var msecHeading = document.getElementById("msec");
+function timer(){
+    msec++;
+    msecHeading.innerHTML = msec;
+    if (msec >= 100){
+        sec++;
+        secHeading.innerHTML = sec;
+        msec = 0;
+    }
+    else if (sec >= 60){
+        min++;
+        sec = 0;
+        minHeading.innerHTML = min;
+    }
+}
+function Start(){
+    interval = setInterval(timer,10);
+}
+function Stop(){
+    clearInterval(interval)
+}
+function Reset(){
+    min = 0;
+    msec = 0;
+    sec = 0;
+    minHeading.innerHTML = min;
+    secHeading.innerHTML = sec;
+    msecHeading.innerHTML = msec;
+    stop();
+}
